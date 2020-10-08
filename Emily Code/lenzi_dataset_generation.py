677e99aeba16a9da280bd206452909b34a235d05
emilymak <47035678+emilymak@users.noreply.github.com>
2020-10-08 14:34:14 -0400

    amgen mAb fasta

--- Emily Code/lenzi_dataset_generation.py
+++ Emily Code/lenzi_dataset_generation.py
@@ -11,14 +11,14 @@ import pandas as pd
 lenzi_pos_reps_1 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_pos_reps_1.csv", header = 0, index_col = 0)
 lenzi_pos_reps_2 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_pos_reps_2.csv", header = 0, index_col = 0)
 lenzi_pos_reps_3 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_pos_reps_3.csv", header = 0, index_col = 0)
-lenzi_pos_reps_4 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_pos_reps_4.csv", header = 0, index_col = 0) 
+#lenzi_pos_reps_4 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_pos_reps_4.csv", header = 0, index_col = 0) 
 
 lenzi_neg_reps_1 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_neg_reps_1.csv", header = 0, index_col = 0)
 lenzi_neg_reps_2 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_neg_reps_2.csv", header = 0, index_col = 0)
 lenzi_neg_reps_3 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_neg_reps_3.csv", header = 0, index_col = 0)
 lenzi_neg_reps_4 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_neg_reps_4.csv", header = 0, index_col = 0)
 
-lenzi_reps = pd.concat([lenzi_pos_reps_1, lenzi_pos_reps_2, lenzi_pos_reps_3, lenzi_pos_reps_4, lenzi_neg_reps_1, lenzi_neg_reps_2, lenzi_neg_reps_3, lenzi_neg_reps_4], axis = 0)
+lenzi_reps = pd.concat([lenzi_pos_reps_1, lenzi_pos_reps_2, lenzi_pos_reps_3, lenzi_neg_reps_1, lenzi_neg_reps_2, lenzi_neg_reps_3, lenzi_neg_reps_4], axis = 0)
 lenzi_reps.to_csv('lenzi_reps.csv', header = True, index = True)
 
 lenzi_pos_seqs = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_pos_seqs.txt", header = None, index_col = 0)
@@ -29,14 +29,14 @@ lenzi_seqs.to_csv('lenzi_seqs.csv', header = True, index = True)
 lenzi_pos_finalhidden_1 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_pos_finalhidden_1.csv", header = 0, index_col = 0)
 lenzi_pos_finalhidden_2 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_pos_finalhidden_2.csv", header = 0, index_col = 0)
 lenzi_pos_finalhidden_3 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_pos_finalhidden_3.csv", header = 0, index_col = 0)
-lenzi_pos_finalhidden_4 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_pos_finalhidden_4.csv", header = 0, index_col = 0) 
+#lenzi_pos_finalhidden_4 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_pos_finalhidden_4.csv", header = 0, index_col = 0) 
 
 lenzi_neg_finalhidden_1 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_neg_finalhidden_1.csv", header = 0, index_col = 0)
 lenzi_neg_finalhidden_2 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_neg_finalhidden_2.csv", header = 0, index_col = 0)
 lenzi_neg_finalhidden_3 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_neg_finalhidden_3.csv", header = 0, index_col = 0)
 lenzi_neg_finalhidden_4 = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_neg_finalhidden_4.csv", header = 0, index_col = 0)
 
-lenzi_finalhidden = pd.concat([lenzi_pos_finalhidden_1, lenzi_pos_finalhidden_2, lenzi_pos_finalhidden_3, lenzi_pos_finalhidden_4, lenzi_neg_finalhidden_1, lenzi_neg_finalhidden_2, lenzi_neg_finalhidden_3, lenzi_neg_finalhidden_4], axis = 0)
+lenzi_finalhidden = pd.concat([lenzi_pos_finalhidden_1, lenzi_pos_finalhidden_2, lenzi_pos_finalhidden_3, lenzi_neg_finalhidden_1, lenzi_neg_finalhidden_2, lenzi_neg_finalhidden_3, lenzi_neg_finalhidden_4], axis = 0)
 lenzi_finalhidden.to_csv('lenzi_finalhidden.csv', header = True, index = True)
 
 lenzi_pos_seqs = pd.read_csv("C:\\Users\\makow\\Documents\\GitHub\\UniRep\\lenzi_pos_seqs.txt", header = None, index_col = 0)
